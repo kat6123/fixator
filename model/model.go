42240ecb0b35779026b58c58ea3a12f8ac5e411e
Ekaterina Shilovskaya <shilovskayakaterina@yandex.ru>
2020-08-14 15:02:47 +0300

    Added model for fixation message.

---
+++ model/model.go
@@ -0,0 +1,51 @@
+package model
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+type (
+	FixationTime  time.Time
+	FixationFloat float64
+)
+
+type Fixation struct {
+	Datetime FixationTime  `json:"Дата и время фиксации"`
+	Car      string        `json:"Номер транспортного средства"`
+	Velocity FixationFloat `json:"Скорость движения км/ч,string"`
+}
+
+const layout = "\"02.01.2006 15:04:05\""
+
+func (t *FixationTime) UnmarshalJSON(b []byte) error {
+	parsedTime, err := time.Parse(layout, string(b))
+	if err != nil {
+		return fmt.Errorf("parse order time: %v", err)
+	}
+	*t = FixationTime(parsedTime)
+	return nil
+}
+
+func (t FixationTime) MarshalJSON() ([]byte, error) {
+	return []byte(time.Time(t).Format(layout)), nil
+}
+
+func (f *FixationFloat) UnmarshalJSON(b []byte) error {
+	b = bytes.Replace(b, []byte{','}, []byte{'.'}, 1)
+	parsed, err := strconv.ParseFloat(string(b), 64)
+	if err != nil {
+		return fmt.Errorf("parse order time: %v", err)
+	}
+	*f = FixationFloat(parsed)
+	return nil
+}
+
+func (f FixationFloat) MarshalJSON() ([]byte, error) {
+	parsed := []byte(
+		`"` + strconv.FormatFloat(float64(f), 'g', -1, 64) + `"`)
+	b := bytes.Replace(parsed, []byte{'.'}, []byte{','}, 1)
+	return b, nil
+}
---
+++ model/model_test.go
@@ -0,0 +1,115 @@
+package model
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFixationFloat_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name       string
+		f          FixationFloat
+		want       string
+		wantErrMsg string
+	}{
+		{
+			name: "marshal 65.5",
+			f:    65.5,
+			want: "\"65,5\"",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.f.MarshalJSON()
+			if err != nil {
+				assert.EqualError(t, err, tt.wantErrMsg)
+			}
+
+			assert.Equal(t, tt.want, string(got))
+		})
+	}
+}
+
+func TestFixationFloat_UnmarshalJSON(t *testing.T) {
+	tests := []struct {
+		name       string
+		b          []byte
+		f          FixationFloat
+		wantErrMsg string
+	}{
+		{
+			name: "unmarshal \"65,5\"",
+			b:    []byte("65,5"),
+			f:    65.5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var gotF FixationFloat
+			if err := gotF.UnmarshalJSON(tt.b); err != nil {
+				assert.EqualError(t, err, tt.wantErrMsg)
+			}
+
+			assert.Equal(t, tt.f, gotF)
+		})
+	}
+}
+
+func TestFixationTime_MarshalJSON(t *testing.T) {
+	tests := []struct {
+		name       string
+		time       string
+		want       []byte
+		wantErrMsg string
+	}{
+		{
+			name: "marshal",
+			time: "20.12.2019 14:31:25",
+			want: []byte("\"20.12.2019 14:31:25\""),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parsedTime, err := time.Parse(layout, tt.time)
+			if err != nil {
+				fmt.Printf("parse order time: %v", err)
+				return
+			}
+			got, err := parsedTime.MarshalJSON()
+			if err != nil {
+				assert.EqualError(t, err, tt.wantErrMsg)
+			}
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func TestFixationTime_UnmarshalJSON(t *testing.T) {
+	const layout = "02.01.2006 15:04:05"
+	tests := []struct {
+		name       string
+		b          []byte
+		parsedTime string
+		wantErrMsg string
+	}{
+		{
+			name:       "unmarshal",
+			b:          []byte("\"20.12.2019 14:31:25\""),
+			parsedTime: "20.12.2019 14:31:25",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var gotT FixationTime
+			if err := gotT.UnmarshalJSON(tt.b); err != nil {
+				assert.EqualError(t, err, tt.wantErrMsg)
+			}
+
+			assert.Equal(t, tt.parsedTime, time.Time(gotT).Format(layout))
+		})
+	}
+}
